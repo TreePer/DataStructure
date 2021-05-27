bc58a21073265478c0432b2a62d8cb3bb5b8ab92
TreePer <treeper0552@gmail.com>
2021-05-28 00:11:48 +0900

    Add files via upload

---
+++ 0528-sort/BubbleSort/BubbleTest.java
@@ -0,0 +1,15 @@
+package BubbleSort;
+
+public class BubbleTest {
+	public static void main(String args[]) {
+		int a[] = {69, 10, 30, 2, 16, 8, 31, 22};
+		BubbleSort S = new BubbleSort();
+		System.out.printf(" \n정렬할 원소 : ");
+		for(int i = 0; i < a.length; i++) {
+			System.out.printf(" %d", a[i]);
+		}
+		System.out.println();
+		S.BubbleSort(a);
+	}
+
+}
